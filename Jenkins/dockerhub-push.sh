8c0cef32d31edd542757643d10785a3075e2dd96
mro- <marlen.rotert@gmail.com>
2015-11-18 16:57:24 +0100

    Update dockerhub-push.sh

--- Jenkins/dockerhub-push.sh
+++ Jenkins/dockerhub-push.sh
@@ -4,7 +4,7 @@
 /home/ec2-user/docker-latest login -e ${dockerhub_mail} -u ${dockerhub_user} -p ${dockerhub_pwd}
 
 #Tag images
-/home/ec2-user/docker-latest tag -f moviedatabase_monitoring cdzwei/mvdb_monitoring:$(git log | head -1 | sed s/'commit '//)
+/home/ec2-user/docker-latest tag -f cdzwei/mvdb_monitoring cdzwei/mvdb_monitoring:$(git log | head -1 | sed s/'commit '//)
 
 # Push to DockerHub
 /home/ec2-user/docker-latest push docker.io/cdzwei/mvdb_monitoring
